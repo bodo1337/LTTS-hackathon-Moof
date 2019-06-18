3bba76363972a7fe49483e6820dd865766e7a921
Bruno Krauss <info@brunokrauss.com>
2019-06-18 14:37:43 +0200

    iinit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ListPage } from '../list/list';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  historyList = [{
+    from: "Zürich, Hauptbahnhof",
+    to: "Zürich, Morgental"
+  }, {
+    from: "Zürich, Coach Park",
+    to: "Zürich, Teifenbrunnen"
+  },{
+    from: "Zürich, Chinagarten",
+    to: "Zürich, Letzigrund"
+  }];
+
+  from = "";
+  to = "";
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  openHistory(history) {
+    console.log(history);
+    this.from = history.from;
+    this.to = history.to;
+  }
+
+  calulcateCommuting() {
+    this.navCtrl.push(ListPage);
+  }
 }
